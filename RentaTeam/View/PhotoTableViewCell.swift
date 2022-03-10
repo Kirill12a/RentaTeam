2e16860533d71b00aee8dad11a208ed0d4e4f490
Kirill <kirilldrozd12@gmail.com>
2022-03-11 00:48:09 +0300

    Передела на SnapKit, но коллекция на якорях

--- RentaTeam/View/PhotoTableViewCell.swift
+++ RentaTeam/View/PhotoTableViewCell.swift
@@ -7,64 +7,68 @@
 
 import UIKit
 import SDWebImage
+import SnapKit
 
 class PhotoTableViewCell: UITableViewCell {
 
-    static let reuseID = "Cell"
+  static let reuseID = "Cell"
 
-    let photoImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .scaleAspectFit
-        return imageView
-    }()
+  let photoImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    imageView.contentMode = .scaleAspectFit
+    return imageView
+  }()
 
-    let userNameLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont(name: "Apple SD Gothic Neo", size: 17)
-        label.textAlignment = .left
-        label.numberOfLines = 0
-        return label
-    }()
+  let userNameLabel: UILabel = {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.font = UIFont(name: "Apple SD Gothic Neo", size: 17)
+    label.textAlignment = .left
+    label.numberOfLines = 0
+    return label
+  }()
 
-    var photo: FavouritePhoto!{
-        didSet {
-            guard let photoURL = URL(string: photo.photoUrl) else {return}
-            photoImageView.sd_setImage(with: photoURL, completed: nil)
-            userNameLabel.text = "Made by: \(photo.userName)"
-        }
+  var photo: FavouritePhoto!{
+    didSet {
+      guard let photoURL = URL(string: photo.photoUrl) else {return}
+      photoImageView.sd_setImage(with: photoURL, completed: nil)
+      userNameLabel.text = "Made by: \(photo.userName)"
     }
+  }
 
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        photoImageView.image = nil
-    }
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    photoImageView.image = nil
+  }
 
-    private func setUpImageView() {
-        addSubview(photoImageView)
-        photoImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
-        photoImageView.topAnchor.constraint(equalTo: topAnchor, constant: 5).isActive = true
-        photoImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5).isActive = true
-        photoImageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        photoImageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+  private func setUpImageView() {
+    addSubview(photoImageView)
+    photoImageView.snp.makeConstraints { make in
+      make.left.equalTo(self.contentView).offset(16)
+      make.top.equalTo(self.contentView).offset(5)
+      make.bottom.bottom.equalTo(self.contentView).inset(-5)
+      make.width.height.equalTo(50)
     }
+  }
 
-    private func setUpLabel() {
-        addSubview(userNameLabel)
-        userNameLabel.widthAnchor.constraint(greaterThanOrEqualToConstant: 250).isActive = true
-        userNameLabel.leadingAnchor.constraint(equalTo: photoImageView.trailingAnchor, constant: 20).isActive = true
-        userNameLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        userNameLabel.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+  private func setUpLabel() {
+    addSubview(userNameLabel)
+    userNameLabel.snp.makeConstraints { make in
+      make.width.equalTo(250)
+      make.left.equalTo(photoImageView.snp_rightMargin).offset(20)
+      make.top.equalTo(self.contentView)
+      make.bottom.equalTo(self.contentView)
     }
+  }
 
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setUpImageView()
-        setUpLabel()
-    }
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    setUpImageView()
+    setUpLabel()
+  }
 
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 }
